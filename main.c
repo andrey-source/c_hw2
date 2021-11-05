2c8bf35b43de307ad9fd7ab7d69cc4a5abc071ab
Andrew <andrey_kuzmenko2907@mail.ru>
2021-11-06 00:57:54 +0300

    fixed VLA

--- main.c
+++ main.c
@@ -9,21 +9,23 @@
 
 
 
-// void print_matrix(const double *matrix, const size_t n_rows, const size_t n_columns)
-// {
-//     for (size_t i = 0; i < n_rows; i++)
-//     {   
-//         for (size_t j = 0; j < n_columns; j++)
-//             printf("%0.1lf\t", matrix[i * n_columns + j]);
-//         printf("\n");
-//     }
-//     printf("\n");
-// }
+
+
+void print_matrix(const double *matrix, const size_t n_rows, const size_t n_columns)
+{
+    for (size_t i = 0; i < n_rows; i++)
+    {   
+        for (size_t j = 0; j < n_columns; j++)
+            printf("%0.1lf\t", matrix[i * n_columns + j]);
+        printf("\n");
+    }
+    printf("\n");
+}
 
 int main()
 {
     FILE *f;
-    f = fopen("../test_data.txt", "r");
+    f = fopen("../test_10x5.txt", "r");
     if (!f)
         return EXIT_FAILURE;
 
@@ -31,10 +33,11 @@ int main()
     size_t n_rows = 0;
     size_t n_columns = 0;
 
+
+
     input(&matrix, &n_rows, &n_columns, f);
 
-
-    // print_matrix(matrix, n_rows, n_columns);
+    print_matrix(matrix, n_rows, n_columns);
 
     
     struct timespec start, finish;
@@ -49,22 +52,23 @@ int main()
         return EXIT_FAILURE;
     }
 
+    print_matrix(matrix, n_rows, n_columns);
     bool flag_finish = clock_gettime(CLOCK_REALTIME, &finish);
     if (!flag_start && !flag_finish)
     {
-        size_t spent_time = 1000000000*(finish.tv_sec - start.tv_sec)+(finish.tv_nsec - start.tv_nsec);
+        size_t spent_time = 1000000000*(finish.tv_sec - start.tv_sec) + (finish.tv_nsec - start.tv_nsec);
         printf("NAIVE TRANSPOSE SPENT TIME: %lu ns \n", spent_time);
     }
     
 
     
-
     void *library = dlopen("./libparallel_transpose_lib.so", RTLD_LAZY);
     if (!library)
     {
         free(matrix);
         return EXIT_FAILURE;
     } 
+
     
     bool(*parallel_transpose)(double *mat, size_t * rows, size_t* columns) = dlsym(library, "parallel_transpose");
     if (!parallel_transpose) 
@@ -85,11 +89,11 @@ int main()
     flag_finish = clock_gettime(CLOCK_REALTIME, &finish);
     if (!flag_start && !flag_finish)
     {
-        size_t spent_time = 1000000000*(finish.tv_sec - start.tv_sec)+(finish.tv_nsec - start.tv_nsec);
+        size_t spent_time = 1000000000*(finish.tv_sec - start.tv_sec) + (finish.tv_nsec - start.tv_nsec);
         printf("NAIVE TRANSPOSE SPENT TIME: %lu ns \n", spent_time);
     }
 
-    // print_matrix(matrix, n_rows, n_columns);
+    print_matrix(matrix, n_rows, n_columns);
     free(matrix);
     return EXIT_SUCCESS;
 }
--- parallel_transpose.c
+++ parallel_transpose.c
@@ -6,6 +6,7 @@
 
 
 
+
 typedef struct args
 {
     double * matrix;
@@ -18,16 +19,18 @@ typedef struct args
 
 
 
-args fill_args(double * matrix, double *copy_matrix, size_t left, size_t right, size_t n_rows, size_t n_columns)
+bool fill_args(args * struct_args, double * matrix, double *copy_matrix, size_t left, size_t right, size_t n_rows, size_t n_columns)
 {
-    args struct_args;
-    struct_args.matrix = matrix;
-    struct_args.copy_matrix = copy_matrix;
-    struct_args.left = left;
-    struct_args.right = right;
-    struct_args.n_columns = n_columns;
-    struct_args.n_rows = n_rows;
-    return struct_args;
+    if (!struct_args || !matrix || !copy_matrix || !n_rows || !n_columns)
+        return false;
+    
+    struct_args->matrix = matrix;
+    struct_args->copy_matrix = copy_matrix;
+    struct_args->left = left;
+    struct_args->right = right;
+    struct_args->n_columns = n_columns;
+    struct_args->n_rows = n_rows;
+    return true;
 
 }
 
@@ -68,34 +71,41 @@ bool parallel_transpose(double * matrix, size_t *n_rows, size_t *n_columns)
 
     size_t num_proc = sysconf(_SC_NPROCESSORS_ONLN);
     size_t num_pth = num_proc < size ? num_proc : size;
-    pthread_t pth[num_pth- 1];
+
+
+    pthread_t* pth = (pthread_t*)malloc((num_pth- 1) * sizeof(pthread_t));
     size_t size_butch = size / num_pth;
-    args args_butch[num_pth];
+
+    args* args_butch = (args*)malloc(num_pth * sizeof(args));
     for (size_t i =  0; i < num_pth - 1; i ++)
     {
-        args_butch[i] = fill_args(matrix,
-                                copy_matrix,
-                                 size_butch * i, 
-                                 size_butch * (i +1),
-                                 *n_rows, 
-                                 *n_columns);
+        bool fill_flag = fill_args(&args_butch[i], matrix, copy_matrix, size_butch * i,
+                                    size_butch * (i +1), *n_rows, *n_columns);
+        if (!fill_flag)
+        {
+            free(pth);
+            free(copy_matrix);
+            free(args_butch);
+            return false;
+        }                               
     }
-    args_butch[num_pth - 1] = fill_args(matrix,
-                                        copy_matrix,
-                                        size_butch * (num_pth - 1),
-                                        size,
-                                        *n_rows,
-                                        *n_columns);
+    bool fill_flag = fill_args(&args_butch[num_pth - 1], matrix, copy_matrix, 
+                                size_butch * (num_pth - 1),  size, *n_rows, *n_columns);
+    if (!fill_flag)
+        return false;
 
     for (size_t i = 0; i < num_pth - 1; i ++)
-    {
-        
+    { 
         int flag_error = pthread_create(&pth[i], NULL, butch_transpose, (void*)&args_butch[i]);
         if (flag_error)
         {
-           return false;
+            free(pth);
+            free(copy_matrix);
+            free(args_butch);
+            return false;
         }
     }
+
     void * ptr = (void*)&args_butch[num_pth - 1];
     butch_transpose(ptr);
 
@@ -104,11 +114,16 @@ bool parallel_transpose(double * matrix, size_t *n_rows, size_t *n_columns)
         int status = pthread_join(pth[i], NULL);
         if (status != 0)
             {
+                free(pth);
                 free(copy_matrix);
+                free(args_butch);
                 return(false);
             }
     }
+
     free(copy_matrix);
+    free(pth);
+    free(args_butch);
     size_t temp = *n_rows;
     *n_rows = *n_columns;
     *n_columns = temp;
